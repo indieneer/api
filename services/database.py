b9f0317329589432999071dbe699e39b586dc88e
oleh-indieneer <143942015+oleh-indieneer@users.noreply.github.com>
2023-09-07 22:13:12 +0100

    Update database.py

--- services/database.py
+++ services/database.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
-
+from os import environ as env
 
 class Database:
     TIMEOUT_TIME = 500
 
     client = MongoClient(
+        env.get("MONGO_URI"),
         timeoutMS=TIMEOUT_TIME,
         retryWrites=False,
         waitQueueTimeoutMS=TIMEOUT_TIME,
