b16a54fe72277b31e9f155c9d41adc912be2f57e
okbmsgnn <75183970+okbmsgnn@users.noreply.github.com>
2023-09-20 21:56:05 +0300

    Added GET /v1/search (#17)

--- api/v1/router.py
+++ api/v1/router.py
@@ -6,6 +6,7 @@ from api.v1.profiles import profiles_controller
 from api.v1.products import products_controller
 from api.v1.logins import logins_controller
 from api.v1.health import health_controller
+from api.v1.search import search_controller
 
 v1_router = Blueprint('v1', __name__, url_prefix='/v1')
 
@@ -14,5 +15,6 @@ v1_router.register_blueprint(products_controller)
 v1_router.register_blueprint(logins_controller)
 v1_router.register_blueprint(health_controller)
 v1_router.register_blueprint(platforms_controller)
+v1_router.register_blueprint(search_controller)
 
 v1_router.register_blueprint(admin_controller)
