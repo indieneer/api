19ae1574ab81199791a9f5b509e03828d29c5262
okbmsgnn <75183970+okbmsgnn@users.noreply.github.com>
2023-09-27 20:26:59 +0300

    Added /v1/search tests (#25)

---
+++ tests/app/api/v1/test_unit_search.py
@@ -0,0 +1,48 @@
+import unittest
+from unittest.mock import patch, Mock
+from app.main import app
+from bson import ObjectId
+from tests.mocks.services import mock_database_connection
+
+class SearchTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    @patch('app.api.v1.search.get_services')
+    def test_search(self, mock_client):
+        mock_client = mock_database_connection(mock_client)
+
+        params1 = {"page": "1", "name": "Counter"}
+        data1 = [
+            {
+                "_id": ObjectId(),
+                "name": "Counter-Strike"
+            },
+            {
+                "_id": ObjectId(),
+                "name": "Counter-Strike Global Offensive"
+            }
+        ]
+
+        meta1 = {
+                "total_count": 2,
+                "items_on_page": 2,
+                "items_per_page": 15,
+                "page_count": 1,
+                "page": 1
+        }
+
+        def aggregate_side_effect():
+            yield [{
+                'items': data1,
+                'count': [{"count": 2}]
+            }]
+
+        mock_client.__getitem__.return_value.aggregate.side_effect = aggregate_side_effect()
+
+        response = self.app.get('/v1/search', query_string=params1)
+
+        self.assertEqual(response.get_json()["data"], data1)
+        self.assertEqual(response.get_json()["meta"], meta1)
