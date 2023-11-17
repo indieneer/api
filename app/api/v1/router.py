1c9e8289a35a686e5810f7fddea682f9a6f84951
okabe <zeptonixx@gmail.com>
2023-11-17 18:50:25 +0200

    Added GET /v1/genres

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -6,6 +6,7 @@ from .profiles import profiles_controller
 from .logins import logins_controller
 from .health import health_controller
 from .search import search_controller
+from .genres import genres_controller
 
 v1_router = Blueprint('v1', __name__, url_prefix='/v1')
 
@@ -14,5 +15,6 @@ v1_router.register_blueprint(logins_controller)
 v1_router.register_blueprint(health_controller)
 v1_router.register_blueprint(platforms_controller)
 v1_router.register_blueprint(search_controller)
+v1_router.register_blueprint(genres_controller)
 
 v1_router.register_blueprint(admin_controller)
