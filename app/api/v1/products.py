f7a087806d77b22047c65ab955bf45104980fb3d
okabe <zeptonixx@gmail.com>
2023-12-15 22:01:24 +0200

    Removed redundant endpoints

--- app/api/v1/products.py
+++ app/api/v1/products.py
@@ -8,45 +8,7 @@ from app.models import get_models, exceptions as models_exceptions
 products_controller = Blueprint('products', __name__, url_prefix='/products')
 
 
-@products_controller.route('/<string:product_id>', methods=["GET"])
-def get_product(product_id: str):
-    """
-    Retrieve a product by its ID.
-
-    :param str product_id: The ID of the product to retrieve.
-    :return: The requested product in JSON format.
-    :raises NotFoundException: If the product with the given ID does not exist.
-    :rtype: dict
-    """
-
-    product_model = get_models(current_app).products
-    product = product_model.get(product_id)
-
-    if product is None:
-        raise models_exceptions.NotFoundException(Product.__name__)
-
-    return respond_success(product.as_json())
-
-
-@products_controller.route('/', methods=["GET"])
-def get_products():
-    """
-    Retrieve all products.
-
-    This endpoint is responsible for fetching all products available in the system.
-    Each product is represented in a JSON format.
-
-    :return: A success response containing a list of products.
-    :rtype: Response
-    """
-
-    products_model = get_models(current_app).products
-    products = [product.as_json() for product in products_model.get_all()]
-
-    return respond_success(products)
-
-
-@products_controller.route('/slug/<string:product_slug>', methods=["GET"])
+@products_controller.route('/<string:product_slug>', methods=["GET"])
 def get_product_by_slug(product_slug: str):
     """
     Retrieve a product by its slug.
