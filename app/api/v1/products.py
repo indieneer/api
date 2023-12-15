ca9fd400598dcbda2de7d374744d08d6f45839f0
okabe <zeptonixx@gmail.com>
2023-12-15 18:26:10 +0200

    Added GET /v1/products/slug/:slug

--- app/api/v1/products.py
+++ app/api/v1/products.py
@@ -44,3 +44,27 @@ def get_products():
     products = [product.as_json() for product in products_model.get_all()]
 
     return respond_success(products)
+
+
+@products_controller.route('/slug/<string:product_slug>', methods=["GET"])
+def get_product_by_slug(product_slug: str):
+    """
+    Retrieve a product by its slug.
+
+    This endpoint is used for fetching a product based on its unique slug.
+    A slug is a human-readable, URL-friendly unique identifier for a product.
+
+    :param str product_slug: The slug of the product to retrieve.
+    :return: The requested product in JSON format.
+    :raises NotFoundException: If the product with the given slug does not exist.
+    :rtype: dict
+    """
+
+    product_model = get_models(current_app).products
+    product = product_model.get_by_slug(product_slug)
+
+    if product is None:
+        raise models_exceptions.NotFoundException(Product.__name__)
+
+    return respond_success(product.as_json())
+
