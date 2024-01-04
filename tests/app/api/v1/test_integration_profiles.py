6d4f64f12f1d8754c2028fc788f2c07793a4b837
Yurii Palamarchuk <yurapalamarchuk111@gmail.com>
2024-01-05 00:41:27 +0100

    Add Integration tests for Profile handler

--- tests/app/api/v1/test_integration_profiles.py
+++ tests/app/api/v1/test_integration_profiles.py
@@ -1,3 +1,7 @@
+import time
+
+from bson import ObjectId
+
 from app.middlewares.requires_auth import create_test_token
 from app.models.profiles import ProfileCreate
 from tests import IntegrationTest
@@ -10,11 +14,12 @@ class ProfilesTestCase(IntegrationTest):
         # given
         profile, cleanup = self.factory.profiles.create(input=ProfileCreate(
             email="test_integration+regular@pork.com",
-            password="9!8@7#6$5%4^3&2*1(0)-_=+[]{}|;:"
+            password="test_integration",
         ))
         self.addCleanup(cleanup)
 
         # when
+        time.sleep(0.5)  # Temporary fix for free plan
         response = self.app.get(
             f"/v1/profiles/{str(profile._id)}")
         response_json = response.get_json()
@@ -22,3 +27,137 @@ class ProfilesTestCase(IntegrationTest):
         # then
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response_json["data"], profile.to_json())
+
+    def test_get_profile_not_found(self):
+        # when
+        time.sleep(0.5)  # Temporary fix for free plan
+        response = self.app.get(f"/v1/profiles/{ObjectId()}")
+
+        # then
+        self.assertEqual(response.get_json()["error"], "\"Profile\" not found.")
+        self.assertEqual(response.status_code, 404)
+
+    def test_create_profile(self):
+        # given
+        profile_data = ProfileCreate(
+            email="test_integration_create@pork.com",
+            password="test_integration")
+
+        # when
+        time.sleep(0.5)  # Temporary fix for free plan
+        response = self.app.post(
+            "/v1/profiles/",
+            json=profile_data.to_json()
+        )
+
+        # then
+        time.sleep(0.5)  # Temporary fix for free plan
+        self.factory.profiles.cleanup(profile_data.email)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.get_json()["data"]["email"], profile_data.email)
+
+    def test_create_profile_with_invalid_body(self):
+        # given
+        profile_data = {
+            "email": "test_email@pork.com"
+        }
+
+        # when
+        time.sleep(0.5)  # Temporary fix for free plan
+        response = self.app.post(
+            "/v1/profiles/",
+            json=profile_data
+        )
+
+        # then
+        self.assertEqual(response.get_json()["error"], "Bad Request.")
+        self.assertEqual(response.status_code, 400)
+
+    def test_update_profile(self):
+        self.skipTest("Profile PATCH is not implemented yet.")
+
+    def test_delete_profile(self):
+        # given
+        profile, cleanup = self.factory.profiles.create(input=ProfileCreate(
+            email="test_user_to_be_deleted@pork.com",
+            password="test_pork_john"))
+        self.addCleanup(cleanup)
+        expected_output = {
+            "status": "ok",
+            "data": {
+                "acknowledged": True,
+            }
+        }
+
+        # when
+        time.sleep(0.5)  # Temporary fix for free plan
+        response = self.app.delete(
+            f"/v1/profiles/{str(profile._id)}",
+            headers={"Authorization": f"Bearer {create_test_token(profile_id=str(profile._id))}"}
+        )
+
+        # then
+        self.assertEqual(response.get_json(), expected_output)
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_profile_not_found(self):
+        # when
+        time.sleep(0.5)  # Temporary fix for free plan
+        profile_id = ObjectId()
+        response = self.app.delete(
+            f"/v1/profiles/{profile_id}",
+            headers={"Authorization": f"Bearer {create_test_token(profile_id=str(profile_id))}"}
+        )
+
+        # then
+        self.assertEqual(response.get_json()["error"], "\"Profile\" not found.")
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_profile_forbidden_when_profile_owned_by_another_user(self):
+        # given
+        profile, cleanup = self.factory.profiles.create(input=ProfileCreate(
+            email="fake_pork@pork.com",
+            password="test_pork_john"))
+        self.addCleanup(cleanup)
+
+        # when
+        time.sleep(0.5)  # Temporary fix for free plan
+        response = self.app.delete(
+            f"/v1/profiles/{str(profile._id)}",
+            headers={"Authorization": f"Bearer {self.token}"}
+        )
+
+        # then
+        self.assertEqual(response.get_json()["error"], "Forbidden.")
+        self.assertEqual(response.status_code, 403)
+
+    def test_get_authenticated_profile(self):
+        # given
+        profile, cleanup = self.factory.profiles.create(input=ProfileCreate(
+            email="me_john_pork@pork.com",
+            password="pork_password"))
+        self.addCleanup(cleanup)
+
+        # when
+        time.sleep(0.5)  # Temporary fix for free plan
+        response = self.app.get(
+            "/v1/profiles/me",
+            headers={"Authorization": f"Bearer {create_test_token(profile_id=str(profile._id))}"}
+        )
+
+        # then
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json()["data"], profile.to_json())
+
+    def test_get_authenticated_profile_not_found(self):
+        # when
+        time.sleep(0.5)
+        response = self.app.get(
+            "/v1/profiles/me",
+            headers={"Authorization": f"Bearer {create_test_token(profile_id=str(ObjectId()))}"}
+        )
+
+        # then
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.get_json()["error"], "\"Profile\" not found.")
