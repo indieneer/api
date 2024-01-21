73268e06ef50f2fe7e8ef0375f91ba30e92dcd2b
oleh-indieneer <143942015+oleh-indieneer@users.noreply.github.com>
2024-01-21 17:29:07 +0000

    Cache auth0 tokens in tests (#56)
    * Cache auth0 tokens in tests
    
    * Update test.yml
    
    * Update .gitignore
    
    * Update test.yml
    
    * Update test.yml
    
    * Cache pip dependencies
    
    * Update test.yml
    
    * Cache pip dependencies in unit tests

--- tests/app/api/v1/admin/test_integration_tags.py
+++ tests/app/api/v1/admin/test_integration_tags.py
@@ -7,8 +7,8 @@ from tests import IntegrationTest
 class TagsTestCase(IntegrationTest):
     @property
     def token(self):
-        return self.models.logins.login(email="test_integration+admin@pork.com",
-                                        password="9!8@7#6$5%4^3&2*1(0)-_=+[]{}|;:")["access_token"]
+        return self.factory.logins.login(email="test_integration+admin@pork.com",
+                                               password="9!8@7#6$5%4^3&2*1(0)-_=+[]{}|;:")["access_token"]
 
     def test_get_tags(self):
         # given
@@ -32,12 +32,13 @@ class TagsTestCase(IntegrationTest):
 
         # then
         self.assertEqual(response.status_code, 401)
-        self.assertEqual(response_json["error"]["code"], "authorization_header_missing")
+        self.assertEqual(response_json["error"]
+                         ["code"], "authorization_header_missing")
 
     def test_get_tags_with_not_admin_token(self):
         # given
-        token = self.models.logins.login(email="test_integration+regular@pork.com",
-                                         password="9!8@7#6$5%4^3&2*1(0)-_=+[]{}|;:")["access_token"]
+        token = self.factory.logins.login(email="test_integration+regular@pork.com",
+                                          password="9!8@7#6$5%4^3&2*1(0)-_=+[]{}|;:")["access_token"]
 
         # when
         response = self.app.get("/v1/admin/tags",
@@ -72,7 +73,8 @@ class TagsTestCase(IntegrationTest):
 
         # then
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(response_json["error"], f"The tag with ID {id} was not found.")
+        self.assertEqual(response_json["error"],
+                         f"The tag with ID {id} was not found.")
 
     def test_get_tag_by_id_with_invalid_token(self):
         # given
@@ -96,7 +98,8 @@ class TagsTestCase(IntegrationTest):
 
         # when
         response = self.app.post("/v1/admin/tags",
-                                 headers={"Authorization": f'Bearer {self.token}'},
+                                 headers={
+                                     "Authorization": f'Bearer {self.token}'},
                                  json=payload)
         response_json = response.get_json()
 
@@ -113,7 +116,8 @@ class TagsTestCase(IntegrationTest):
 
         # when
         response = self.app.post("/v1/admin/tags",
-                                 headers={"Authorization": f'Bearer {self.token}a'},
+                                 headers={
+                                     "Authorization": f'Bearer {self.token}a'},
                                  json=payload)
         response_json = response.get_json()
 
@@ -129,7 +133,8 @@ class TagsTestCase(IntegrationTest):
 
         # when
         response = self.app.post("/v1/admin/tags",
-                                 headers={"Authorization": f'Bearer {self.token}'},
+                                 headers={
+                                     "Authorization": f'Bearer {self.token}'},
                                  json=data)
         response_json = response.get_json()
 
@@ -148,7 +153,8 @@ class TagsTestCase(IntegrationTest):
 
         # when
         response = self.app.patch(f"/v1/admin/tags/{tag._id}",
-                                  headers={"Authorization": f'Bearer {self.token}'},
+                                  headers={
+                                      "Authorization": f'Bearer {self.token}'},
                                   json=payload)
         response_json = response.get_json()
 
@@ -167,7 +173,8 @@ class TagsTestCase(IntegrationTest):
 
         # when
         response = self.app.patch(f"/v1/admin/tags/{tag._id}",
-                                  headers={"Authorization": f'Bearer {self.token}a'},
+                                  headers={
+                                      "Authorization": f'Bearer {self.token}a'},
                                   json=payload)
         response_json = response.get_json()
 
@@ -187,13 +194,15 @@ class TagsTestCase(IntegrationTest):
 
         # when
         response = self.app.patch(f"/v1/admin/tags/{tag._id}",
-                                  headers={"Authorization": f'Bearer {self.token}'},
+                                  headers={
+                                      "Authorization": f'Bearer {self.token}'},
                                   json=payload)
         response_json = response.get_json()
 
         # then
         self.assertEqual(response.status_code, 422)
-        self.assertEqual(response_json["error"], "The keys ['description'] are not allowed.")
+        self.assertEqual(
+            response_json["error"], "The keys ['description'] are not allowed.")
 
     def test_update_token_with_invalid_id(self):
         # given
@@ -204,7 +213,8 @@ class TagsTestCase(IntegrationTest):
 
         # when
         response = self.app.patch(f"/v1/admin/tags/{id}",
-                                  headers={"Authorization": f'Bearer {self.token}'},
+                                  headers={
+                                      "Authorization": f'Bearer {self.token}'},
                                   json=payload)
         response_json = response.get_json()
 
@@ -224,14 +234,15 @@ class TagsTestCase(IntegrationTest):
 
         # then
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_json["data"], f"Successfully deleted the tag with ID {tag._id}")
+        self.assertEqual(
+            response_json["data"], f"Successfully deleted the tag with ID {tag._id}")
 
     def test_delete_tag_without_permission(self):
         # given
         tag, cleanup = self.factory.tags.create(TagCreate(name="Test Tag 1"))
         self.addCleanup(cleanup)
-        token = self.models.logins.login(email="test_integration+regular@pork.com",
-                                         password="9!8@7#6$5%4^3&2*1(0)-_=+[]{}|;:")["access_token"]
+        token = self.factory.logins.login(email="test_integration+regular@pork.com",
+                                          password="9!8@7#6$5%4^3&2*1(0)-_=+[]{}|;:")["access_token"]
 
         # when
         response = self.app.delete(f"/v1/admin/tags/{tag._id}",
