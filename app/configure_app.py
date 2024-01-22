71a37d04f93b8fd08aac348c68cc9eca024ead1e
oleh-pohorilyy <61743263+oleh-pohorilyy@users.noreply.github.com>
2024-01-22 18:26:08 +0000

    Add CORS middleware (#55)
    * Add cors middleware
    
    * Add unit tests for cors middleware
    
    ---------
    
    Co-authored-by: oleh.pohorilyy <oleh.pohorilyy@indieneer.com>

--- app/configure_app.py
+++ app/configure_app.py
@@ -1,7 +1,6 @@
 from os import environ as env
 
 from flask import Flask
-from flask_cors import CORS
 
 from config import app_config
 
@@ -11,13 +10,3 @@ def configure_app(app: Flask):
 
     app.secret_key = env.get("APP_SECRET_KEY")
     app.url_map.strict_slashes = False
-
-    python_env = env.get("PYTHON_ENV")
-
-    origin = "http://localhost:3000"
-    if python_env == "staging":
-        origin = "http://*.indieneer.com"
-    elif python_env == "production":
-        origin = "http://indieneer.com"
-
-    CORS(app, resources={r"/v1/*": {"origins": origin}})
--- app/register_middlewares.py
+++ app/register_middlewares.py
@@ -1,20 +1,57 @@
 import sys
-import os
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask import Flask
+from flask_cors import CORS
+from app.middlewares.cors_middleware import CORSMiddleware
 
 from lib.logger.middleware import LoggerMiddleware, LoggerOptions
 from lib.logger.logger import Logger
 from flask_http_middleware import MiddlewareManager
 from app.middlewares.error_middleware import ErrorMiddleware
 
+
 def register_middlewares(app: Flask):
     # Use the middleware manager, so that we can easily append middlewares
     app.wsgi_app = MiddlewareManager(app)
 
+    ##
+    # CORS
+    ##
+    origins = []
+    match app.config.get("PYTHON_ENV"):
+        case "development":
+            origins = [
+                "http://localhost:3000",
+                "http://localhost:4000"
+            ]
+        case "staging":
+            origins = [
+                "http://staging.indieneer.com",
+                "http://web-*.indieneer.com"
+            ]
+        case "production":
+            origins = ["http://indieneer.com"]
+
+    app.wsgi_app.add_middleware(CORSMiddleware, options=CORS(
+        origins=origins,
+        allow_headers=[
+            "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token",
+            "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"
+        ],
+        expose_headers=["Content-Length", "Access-Control-Allow-Origin"],
+        supports_credentials=True,
+        max_age=timedelta(hours=12),
+        methods=["POST", "GET", "PATCH", "PUT", "DELETE"]
+    ))
+
+    ##
+    # Logger
+    ##
+
+    # Disable default logger
+    logging.getLogger('werkzeug').level = logging.WARN
     # Add logger to non-test environments
-    logging.getLogger('werkzeug').disabled = True
     if app.config["ENVIRONMENT"] != "test":
         logger = Logger(sys.stdout, app.config["ENVIRONMENT"] == "development")
         logger_options = LoggerOptions(
@@ -27,6 +64,10 @@ def register_middlewares(app: Flask):
                 "server.version": f"flask/{app.config.get('VERSION')}"
             }
         )
-        app.wsgi_app.add_middleware(LoggerMiddleware, logger=logger, options=logger_options)
+        app.wsgi_app.add_middleware(
+            LoggerMiddleware, logger=logger, options=logger_options)
 
+    ##
+    # Error
+    ##
     app.wsgi_app.add_middleware(ErrorMiddleware)
