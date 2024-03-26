c916a97df19cc90d3422509b170449f0693e3963
Yurii Palamarchuk <yurapalamarchuk111@gmail.com>
2024-03-26 19:46:16 +0100

    Fix tests

--- tests/app/api/v1/test_integration_products.py
+++ tests/app/api/v1/test_integration_products.py
@@ -1,10 +1,7 @@
-from lib.constants import strong_password
 from tests import IntegrationTest
 
 
 class ProductsTestCase(IntegrationTest):
-
-    # Tests for getting
     def test_get_product_by_slug(self):
         # when
         response = self.app.get(
@@ -12,22 +9,15 @@ class ProductsTestCase(IntegrationTest):
         )
 
         actual = response.get_json().get("data")
-        print(response.get_json())
+
         # then
         self.assertEqual(response.status_code, 200)
         self.assertEqual(actual.get("name"), "Geometry Dash")
 
     def test_fails_to_get_a_nonexistent_product(self):
-        # given
-        regular_user = self.fixtures.regular_user
-
-        tokens = self.factory.logins.login(
-            regular_user.email, "9!8@7#6$5%4^3&2*1(0)-_=+[]{}|;:")
-
         # when
         response = self.app.get(
-            f'/v1/products/this-will-never-exist-hj43hj32b3wejfdns',
-            headers={"Authorization": f'Bearer {tokens.id_token}'}
+            f'/v1/products/this-will-never-exist-hj43hj32b3wejfdns'
         )
 
         actual = response.get_json()
