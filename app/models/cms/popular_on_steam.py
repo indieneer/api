d8333e906e7869d763e3afdf6c4b94015f0ffcd7
Yurii Palamarchuk <yurapalamarchuk111@gmail.com>
2024-06-14 18:49:13 +0200

    Enhance create and patch methods to automatically increase/decrease potentially affected order_indexes.
    For example, if there are 5 records, and we're placing the 6th one on the second place. So in this case, we should increase the order_index field of all the previous records by 1 starting from 2.
    
    Use transactions for sustainability.

--- app/models/cms/popular_on_steam.py
+++ app/models/cms/popular_on_steam.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing import List, Optional
 
 from bson import ObjectId
+from pymongo.errors import OperationFailure
 
 from app.models.base import BaseDocument
 from app.services import Database
@@ -42,33 +43,76 @@ class PopularOnSteamModel:
     def get_all(self) -> List[PopularOnSteam]:
         products = []
 
-        for product in self.db.connection[self.collection].find():
+        for product in self.db.connection[self.collection].find().sort("order_index", 1):
             products.append(PopularOnSteam(**product))
 
         return products
 
     def create(self, input_data: PopularOnSteamCreate) -> PopularOnSteam:
-        popular_on_steam = PopularOnSteam(**input_data.to_bson())
+        with self.db.client.start_session() as session:
+            with session.start_transaction():
+                try:
+                    existing_index = self.db.connection[self.collection].find_one(
+                        {"order_index": input_data.order_index},
+                        session=session)
+                    if existing_index:
+                        # If the order_index exists, increment all subsequent order_indexes by 1
+                        self.db.connection[self.collection].update_many(
+                            {"order_index": {"$gte": input_data.order_index}},
+                            {"$inc": {"order_index": 1}},
+                            session=session
+                        )
 
-        self.db.connection[self.collection].insert_one(popular_on_steam.to_bson())
-
-        return popular_on_steam
+                    popular_on_steam = PopularOnSteam(**input_data.to_bson())
+                    self.db.connection[self.collection].insert_one(popular_on_steam.to_bson(), session=session)
+                    return popular_on_steam
+                except OperationFailure:
+                    session.abort_transaction()
+                    raise Exception("Insertion failed, transaction aborted")
 
     def patch(self, popular_on_steam_id: str, input_data: PopularOnSteamPatch) -> Optional[PopularOnSteam]:
-        updates = {key: value for key, value in input_data.to_json().items() if value is not None}
-        updates["updated_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.db.connection[self.collection].update_one(
-            {"_id": ObjectId(popular_on_steam_id)},
-            {"$set": updates}
-        )
+        with self.db.client.start_session() as session:
+            with session.start_transaction():
+                try:
+                    current_data = self.db.connection[self.collection].find_one(
+                        {"_id": ObjectId(popular_on_steam_id)}, session=session
+                    )
+                    if not current_data:
+                        return None
 
-        updated_popular_on_steam_data = self.db.connection[self.collection].find_one(
-            {"_id": ObjectId(popular_on_steam_id)}
-        )
-        if updated_popular_on_steam_data:
-            return PopularOnSteam(**updated_popular_on_steam_data)
+                    if input_data.order_index is not None and input_data.order_index != current_data['order_index']:
+                        new_index = input_data.order_index
+                        old_index = current_data['order_index']
 
-        return None
+                        if new_index > old_index:
+                            self.db.connection[self.collection].update_many(
+                                {"order_index": {"$lte": new_index, "$gt": old_index}},
+                                {"$inc": {"order_index": -1}},
+                                session=session
+                            )
+                        else:
+                            self.db.connection[self.collection].update_many(
+                                {"order_index": {"$lt": old_index, "$gte": new_index}},
+                                {"$inc": {"order_index": 1}},
+                                session=session
+                            )
+
+                    updates = {key: value for key, value in input_data.to_json().items() if value is not None}
+                    updates["updated_at"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    self.db.connection[self.collection].update_one(
+                        {"_id": ObjectId(popular_on_steam_id)},
+                        {"$set": updates},
+                        session=session
+                    )
+
+                    updated_popular_on_steam_data = self.db.connection[self.collection].find_one(
+                        {"_id": ObjectId(popular_on_steam_id)},
+                        session=session
+                    )
+                    return PopularOnSteam(**updated_popular_on_steam_data) if updated_popular_on_steam_data else None
+                except OperationFailure:
+                    session.abort_transaction()
+                    raise Exception("Update failed, transaction aborted.")
 
     def delete(self, popular_on_steam_id: str) -> int:
         popular_on_steam = self.db.connection[self.collection].delete_one({"_id": ObjectId(popular_on_steam_id)})
