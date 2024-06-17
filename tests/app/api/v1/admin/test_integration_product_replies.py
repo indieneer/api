457eac731cc4a5f4cdf0c4e8cc929c79fa8d4668
okabe <zeptonixx@gmail.com>
2024-06-17 22:45:59 +0300

    removed a print() call used for debugging

--- tests/app/api/v1/admin/test_integration_product_replies.py
+++ tests/app/api/v1/admin/test_integration_product_replies.py
@@ -98,8 +98,6 @@ class ProductReplyTestCase(IntegrationTest):
                                  headers={"Authorization": f'Bearer {self.token}'}, json=payload)
         response_json = response.get_json()
 
-        print(response_json)
-
         # then
         self.assertEqual(response.status_code, 400)
         self.assertIn("text", response_json["error"])
